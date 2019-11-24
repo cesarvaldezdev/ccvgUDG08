370b884918bf33976ba587d70feca3464655595b
MatchaFierce <cesarvaldez.work@gmail.com>
2019-11-23 19:42:33 -0600

    New Product option added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 
 const url = require('url');
 const path = require('path');
 
+if (process.env.NODE_ENV !== 'production') {
+    require('electron-reload')(__dirname, {
+        electron: path.join(__dirname, '../node_modules', '.bin', 'electron')
+    });
+}
+
 let mainWindow;
+let newProduct;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
@@ -12,4 +19,43 @@ app.on('ready', () => {
         protocol: 'file',
         slashes: true
     }));
+
+    const mainMenu = Menu.buildFromTemplate(templateMenu);
+    Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('closed', () => {
+        app.quit();
+    });
 });
+
+function createNewProductWindow() {
+    newProductWindow = new BrowserWindow({
+        width: 400,
+        height: 330,
+        title: 'Add a New Product'
+    });
+    newProductWindow.setMenu(null);
+    newProductWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'views/new-product.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+    newProductWindow.on('closed', () => {
+        newProductWindow = null;
+    });
+}
+
+const templateMenu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Product',
+                accelerator: 'Ctrl+N',
+                click() {
+                    createNewProductWindow();
+                }
+            }
+        ]
+    }
+];
